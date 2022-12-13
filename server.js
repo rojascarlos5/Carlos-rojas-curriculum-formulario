b3847394b7c399d70687ad9f49d0a9cc42cb5671
rojascarlos5 <ggggoglez2@gmail.com>
2022-12-13 00:55:19 -0400

    nuevo cambio

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const PassportLocal = require("passport-local").Strategy;
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
